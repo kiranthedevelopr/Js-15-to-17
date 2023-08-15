dcf0d2b5bc729bf26d0bc13a9fe6325f86a92a3f
kiranthedevelopr <kirannasim05@gmail.com>
2023-08-15 07:01:54 -0700

    Assignment

---
+++ 17 to 20/apps.js
@@ -0,0 +1,181 @@
+//Question # 1
+ //let array = [[ ],[ ] ]
+
+
+//Question # 2
+//let array_1 = [[0,1,2,3],
+//[1,0,1,2],[2,1,0,1]]
+
+//Question # 3
+//for(let i=1; i<=10; i++) {
+    //console.log(i);
+//}
+
+//Question # 4
+ //let number = +prompt("enter a no")
+ //let len = +prompt("enter length")
+ //console.log ("multiplication table of" , number);
+ //for (let i = 1 ; i<=len;i++)
+ //{
+ //console.log(number+ "X"+i+"="+number*i);
+//}
+
+//Question # 5
+//let fruits = ["apple","banana","mango","orange","strawberry"]
+//for (let i=0;
+//i<fruits.length; i++){
+//console.log("element at index",i,"is",fruits[i]);
+//}
+
+//Question # 6
+//document.write("<h1>counting</h1>");
+//for (let i=1; i<=14;i++){
+   // document.write(i,",")
+//}
+ 
+//for (let i = 15; i<=15;i++){
+    //document.write(i,)
+//}
+
+
+
+//document.write("<h1> reverse counting</h1>");
+//for (let i =10; i>=2;i--){
+    //document.write(i,",")
+//}
+
+
+//for(let i=1;i>=1;i--){
+  //  document.write(i)
+//}
+
+
+
+//document.write("<h1> even </h1>");
+//for (let i = 0; i<18;i++){
+    //if(i%2===0){
+       // document.write(i,",")
+//}
+//}
+//for (let i = 19; i<=20;i++)
+//{
+//if (i%2===0){
+//document.write(i)
+//}
+//}
+
+
+//document.write("<h1>odd</h1>");
+//for (let i = 1; i<=18;i++){
+    //if(i%2!==0){
+      //  document.write(i,",")
+   // }
+//}
+
+//for (let i = 19; i<20;i++){
+   // if(i%2!==0){
+  //document.write(i)
+//}
+//}
+
+
+//for(let i =19;i<=20;i++)
+{
+    if(i%2!==0){
+document.write(i)
+}
+//}
+
+
+//document.write("<h1>series</h1>");
+//for(let i = 1;i<=18;i++){
+   // if(i%2===0){
+//document.write(i,"k,")
+//}
+//}
+//for(let i=19; i<=20;i++)
+//{
+    //if(i%2===0){
+//document.write(i,"k") 
+//}
+//}
+
+//Question # 7// 
+//let A=["cake","apple","cookie","chips","patties"]
+
+//let user_input = prompt("enter an item to search");
+//let flag= false;
+//let index = 0;
+
+//for(i = 0 ; i < A.length ; i++)
+//{
+   
+   /// if(A[i] === user_input)
+
+   // flag = true
+   // index = i
+//}
+
+//}
+
+//if (flag){
+    //console.log(A[index],"is available at index",index,"in our bakery");
+//}
+
+//else{
+   // console.log("we are sorry",user_input,"is not available");
+//}
+
+//Question # 8
+// let A[24.53.79.91.12];
+// let temp;
+// console.log("the array items are",A);
+
+// for(let i = 0 ; i<A.lenght-1 ; i++){
+
+// for(let j = i+1; j<A.lenght ; j++){
+//     if (A[i] > A [j]){
+//         temp = A[i];
+//         A[i]=A[j];
+//         A[j]=temp;
+//     }
+//     }
+
+// }
+// console.log("the max no is",A[-1]);
+
+
+//Question # 9
+// let A[24.53.79.91.12];
+// let temp;
+
+// console.log("the array items are",A);
+
+// for(let i = 0 ; i<A.lenght-1 ; i++){
+
+// for(let j = i+1; j<A.lenght ; j++){
+
+//     if (A[i] > A [j]){
+//         temp = A[i];
+//         A[i]=A[j];
+//         A[j]=temp;
+//     }
+
+// }
+// }
+// console.log("the min no is",A[0]);
+
+
+
+//Question # 10
+//for (let i   = 1 ; i<=99;i++){
+   // if(i%5===0){
+       // document.write(i,",");
+    //}
+//}
+//for(let i = 99 ; i<=100i++){
+     //if(i%5===0){
+       // document.write(i);
+     //}
+//}
+
